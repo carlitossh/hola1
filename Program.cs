eb918b15961827fdac6d5b20cd3d0f7a864af671
carlitossh <mejia.pena.carlos.leonardo@gmail.com>
2026-04-05 20:50:37 -0600

    delete-add

--- Program.cs
+++ Program.cs
@@ -1,42 +1,59 @@
-﻿using System.Linq.Expressions;
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Linq.Expressions;
 using System.Reflection.Emit;
+enum Category {Work, Personal, Shopping}
 
 public class Task
 {
+    public int id;
     public required string Title {set; get; }
     public required string Description {set; get; }
-    enum Category {Work, Personal, Shopping}
     public required string Categoria {set; get;}
+    
+    public override string ToString()
+    {
+        return $"ID: {id}, Nombre: {Title}, Descripcion: {Description}, Categoría: {Categoria}";
+    }
 }
+
+
 class MenuSystem
 {
-    Task [] tasks = new Task[5];
+  List <Task> tasks = new List<Task>();
     int numtasks=0;
     static void MostrarMenu()
     {
         Console.WriteLine("Menu!");
 
     }
-    static void sel(int opc)
+    static void sel(int opc, MenuSystem op)
     {
+
         switch (opc)
         {
             case 1:
-            MenuSystem op = new MenuSystem();
             Console.WriteLine("nombre de la tarea");
-            string nombre = Console.ReadLine();
-            Console.WriteLine("nombre de la tarea");
-            Console.WriteLine("nombre de la tarea");
-            Console.WriteLine("nombre de la tarea");
-            Task tarea = new Task{Title=nombre};
-
-            op.addTask(Task);
+            string nombre = Console.ReadLine() ?? "";
+            Console.WriteLine("descripcion de la tarea");
+            string descripcion = Console.ReadLine() ?? "";
+            Console.WriteLine("Categoría de la tarea");
+            string Categoria = Console.ReadLine() ?? "";
+            Task tarea = new Task{Title=nombre, Description=descripcion, Categoria=Categoria};
+            op.addTask(tarea);
             break;
             case 2:
+            op.viewallTasks();
             break;
             case 3:
+            Console.WriteLine("Categoría a buscar:\n");
+            string Cate = Console.ReadLine() ?? "";
+            op.viewallTasksCategory(Cate);
             break;
             case 4:
+            Console.WriteLine("id de la tarea a eliminar:\n");
+            int id = Convert.ToInt16 (Console.ReadLine());
+            op.DeleteTask(id);
             break;
             default:
             Console.WriteLine("elige una opcion valida");
@@ -46,9 +63,22 @@ class MenuSystem
 
 
     }
-    public void addTask(Task tasks)
+    public void DeleteTask(int id)
     {
-        this.tasks[numtasks]=tasks;
+        int k=0;
+        this.tasks.RemoveAt(id);
+        numtasks-=1;
+        foreach (Task i in this.tasks)
+        {
+            i.id = k++;
+        }
+        
+    }
+    public void addTask(Task task)
+    {
+        task.id=numtasks;
+        this.tasks.Add(task);
+        numtasks+=1;
     }
     void viewallTasks()
     {
@@ -67,12 +97,14 @@ class MenuSystem
     }
 
     static void Main(string[] args)
-    {
+    {        
+        MenuSystem op = new MenuSystem();
         int opc= 0;
         do
         {
             MenuSystem.MostrarMenu();
-
+            opc =  Convert.ToInt16 (Console.ReadLine());
+            MenuSystem.sel(opc, op);
         }
         while (opc != 5);
         
